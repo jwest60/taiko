2043ccdfd69493f93fc9fb291a3584c61decf7a2
jwest60 <jwest2178@gmail.com>
2019-03-28 23:40:37 -0400

    added input detection

--- taiko/game_state_play.cpp
+++ taiko/game_state_play.cpp
@@ -14,3 +14,14 @@ void Game_State_Play::get_input()
 {
 	return;
 }
+
+void Game_State_Play::handle_event(sf::Event event)
+{
+	if (event.type != sf::Event::KeyPressed) return;
+
+	if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::F)
+		std::cout << "OUTER\n";
+
+	if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::D)
+		std::cout << "INNER\n";
+}
